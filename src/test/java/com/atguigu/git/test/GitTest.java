bff7868dffbf758b12e7c5ef955baf54c863dcc6
xiefan_global <626868510@qq.com>
2018-03-10 21:58:37 +0800

    first commit

---
+++ src/test/java/com/atguigu/git/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
